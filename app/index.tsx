a523ba80c6e61a4a38897c270c032f5ccdb2ad5d
RianAndiSaputra <221210007@student.mercubuana-yogya.ac.id>
2025-05-03 10:43:23 +0700

    done

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.replace('/home');
+      router.replace('/welcome-page');
     }, 100);
 
     return () => clearTimeout(timer);
